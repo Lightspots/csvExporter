386169c5027b2a5f40755e0a57870219aa713954
Benjamin Leber <grisu118@grisu118.ch>
2017-10-03 19:12:15 +0200

    + Add Bintray Publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,13 @@
+import org.gradle.api.tasks.bundling.Jar
+import java.util.Date
+
 val kotlinVersion = "1.1.51"
 
 plugins {
   kotlin("jvm", "1.1.51")
+  id("com.jfrog.bintray") version ("1.7.3")
+  maven
+  `maven-publish`
 }
 
 group = "ch.grisu118"
@@ -15,4 +21,36 @@ dependencies {
   compile(kotlin("stdlib-jre8", kotlinVersion))
   compile(kotlin("reflect", kotlinVersion))
   testCompile(kotlin("test-junit", kotlinVersion))
+}
+
+bintray {
+  user = if (project.hasProperty("bintrayUser")) project.property("bintrayUser").toString() else ""
+  key = if (project.hasProperty("bintrayApiKey")) project.property("bintrayApiKey").toString() else ""
+  setPublications("Bintray")
+  pkg = PackageConfig()
+  pkg.repo = "kotlin"
+  pkg.name = "csvExporter"
+  pkg.setLicenses("MIT")
+  pkg.vcsUrl = "https://github.com/Grisu118/csvExporter.git"
+  pkg.version = VersionConfig()
+  pkg.version.name = project.version.toString()
+  pkg.version.desc = ""
+  pkg.version.released = Date().toString()
+  pkg.version.vcsTag = project.version.toString()
+}
+
+val sourcesJar by tasks.creating(Jar::class) {
+  classifier = "sources"
+  from(java.sourceSets["main"].allSource)
+}
+
+publishing {
+  publications {
+    (publications) {
+      "Bintray"(MavenPublication::class) {
+        from(components["java"])
+        artifact(sourcesJar)
+      }
+    }
+  }
 }
