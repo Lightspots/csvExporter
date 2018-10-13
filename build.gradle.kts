17928c77f50edc3050f4bf305a6bd20cbcf14559
Benjamin Leber <grisu118@grisu118.ch>
2018-10-13 18:28:51 +0200

    + Update kotlin and gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,15 +3,13 @@ import org.gradle.kotlin.dsl.version
 import org.jetbrains.dokka.gradle.DokkaTask
 import java.util.Date
 
-val kotlinVersion = "1.2.10"
-
 buildscript {
   repositories {
     jcenter()
   }
 
   dependencies {
-    classpath("org.jetbrains.dokka:dokka-gradle-plugin:0.9.15")
+    classpath("org.jetbrains.dokka:dokka-gradle-plugin:0.9.17")
   }
 }
 
@@ -20,24 +18,24 @@ apply {
 }
 
 plugins {
-  kotlin("jvm", "1.2.10")
-  id("com.jfrog.bintray") version ("1.7.3")
+  kotlin("jvm") version ("1.2.71")
+  id("com.jfrog.bintray") version ("1.8.4")
   maven
   `maven-publish`
   jacoco
 }
 
 group = "ch.grisu118"
-version = "1.0.2"
+version = "1.0.3"
 
 repositories {
   mavenCentral()
 }
 
 dependencies {
-  compile(kotlin("stdlib-jre8", kotlinVersion))
-  compile(kotlin("reflect", kotlinVersion))
-  testCompile(kotlin("test-junit", kotlinVersion))
+  compile(kotlin("stdlib-jdk8"))
+  compile(kotlin("reflect"))
+  testCompile(kotlin("test-junit"))
 }
 
 tasks.withType<DokkaTask> {
