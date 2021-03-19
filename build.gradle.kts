8031e045ec5c0f95c82dc99b20b317fe7eb0456f
Benjamin Leber <grisu118@grisu118.ch>
2021-03-19 22:36:24 +0100

    + Add release workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,8 +51,8 @@ publishing {
       name = "GitHubPackages"
       url = uri("https://maven.pkg.github.com/lightspots/csvexporter")
       credentials {
-        username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-        password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+        username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
+        password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
       }
     }
   }
